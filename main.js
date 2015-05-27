d1a7ec1157b42a7d0ce6684880ed16e0114463cb
Shawn Gibson <saljgib@gmail.com>
2015-05-26 22:12:43 -0400

    completed question 2 & 3

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ var avg = sum/(items.length);
 console.log("The average price is" + " " +avg);
 
 //Question 2
+
+var costResult = _.filter(items,
+  function(costList){
+  return Number(costList.price) > 14 && Number(costList.price) < 18;
+
+
+});
+ costResult.forEach(function(item){
+   console.log(item.title);
+ });
+
+
+// Question 3
+var gbpResult = _.filter(items,
+  function(currency){
+    return (currency.currency_code) === "GBP";
+  });
+
+  gbpResult.forEach(function(item){
+    console.log(item.title) + console.log(item.currency_code);
+  });
